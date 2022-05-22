a5114967661c382142b737067ee73bb27800e890
EdinGrech <edin.grech@gmail.com>
2022-05-22 11:13:25 +0200

    local hosting

--- app.py
+++ app.py
@@ -45,6 +45,18 @@ def newDevice(deviceName):
         return jsonify({ "error": response["message"] }), 400
 
 if __name__ == '__main__':
-    ip = socket.gethostbyname(socket.gethostname())
-    print(ip)
-    app.run(debug=True,host = ip)
+    def get_ip():
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(0)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            IP = s.getsockname()[0]
+        except Exception:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+        return IP
+
+    print("Server started on:",get_ip())
+    app.run(debug=True,host = get_ip(), port=5000)
